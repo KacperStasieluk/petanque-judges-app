c4d5c9177e834df0ed3ea113db1583a923d899e1
KacperStasieluk <61713593+KacperStasieluk@users.noreply.github.com>
2023-01-11 21:14:29 +0100

    p

--- petanque/shots/models.py
+++ petanque/shots/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -17,5 +18,5 @@ class Session(models.Model):
     code = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     players = models.ManyToManyField(Player)
-    judges = models.ManyToManyField(Judge)
+    judges = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
     status = models.CharField(max_length=255)
