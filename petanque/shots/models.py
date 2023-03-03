4bfbe8cd865bd19557d9b6bc8873c74ccb54d835
Kacper Stasiełuk <kacperstasieluk@gmail.com>
2023-03-03 08:41:07 +0100

    new

--- petanque/shots/models.py
+++ petanque/shots/models.py
@@ -9,6 +9,9 @@ class Player(models.Model):
     license_number = models.CharField(max_length=255)
     club_name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f'{self.name} {self.surname} ({self.license_number})'
+
 class Judge(models.Model):
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
