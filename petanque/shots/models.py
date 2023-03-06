549f64de184065d242e6c415fc95b94d6c16a3e3
Digi Leader <88141592+D1g1L34d3r@users.noreply.github.com>
2023-03-06 14:41:11 +0100

    new

--- petanque/shots/models.py
+++ petanque/shots/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+class Judge(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    license_number = models.CharField(max_length=255)
 
 class Player(models.Model):
     name = models.CharField(max_length=255)
@@ -9,13 +15,11 @@ class Player(models.Model):
     license_number = models.CharField(max_length=255)
     club_name = models.CharField(max_length=255)
 
+    judge = models.ForeignKey(Judge, null=True, blank=True, on_delete=models.CASCADE)
+    eliminations_score = ArrayField(models.IntegerField(), default=[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, ])
+
     def __str__(self):
         return f'{self.name} {self.surname} ({self.license_number})'
-
-class Judge(models.Model):
-    name = models.CharField(max_length=255)
-    surname = models.CharField(max_length=255)
-    license_number = models.CharField(max_length=255)
 
 class Session(models.Model):
     code = models.CharField(max_length=255)
