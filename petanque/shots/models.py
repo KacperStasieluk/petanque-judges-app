3c1c10b66d37c2486b75948b2079536c2eb694cf
Kacper Stasiełuk <kacperstasieluk@gmail.com>
2023-03-09 21:56:38 +0100

    new

--- petanque/shots/models.py
+++ petanque/shots/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
@@ -15,8 +14,8 @@ class Player(models.Model):
     license_number = models.CharField(max_length=255)
     club_name = models.CharField(max_length=255)
 
-    judge = models.ForeignKey(Judge, null=True, blank=True, on_delete=models.CASCADE)
-    eliminations_score = ArrayField(models.IntegerField(), default=[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, ])
+    judge = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE)
+    eliminations_score = models.CharField(max_length=255, default="-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1")
 
     def __str__(self):
         return f'{self.name} {self.surname} ({self.license_number})'
